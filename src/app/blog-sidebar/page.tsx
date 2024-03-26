13d609d93431b846a7e886e45eb18f2c4f432885
Luis Antonio Arredondo Deanda <99154912+LuisAntonioD@users.noreply.github.com>
2024-03-25 18:47:54 -0600

    Update page.tsx
    add response code 201 in the table

--- src/app/blog-sidebar/page.tsx
+++ src/app/blog-sidebar/page.tsx
@@ -44,7 +44,7 @@ const BlogSidebarPage = () => {
                       <tbody>
                         <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                           <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            <code className="text-green-500">200</code>
+                            <code className="text-green-500">200, 201</code>
                           </th>
                           <td className="px-6 py-4">
                             OK
